ef7934bdc76b501aefee8f3fe31a3cd3bc0cfdb4
mattia.rollo <ste.tia@hotmail.it>
2023-02-03 16:13:20 +0100

    fix route login service provider HOME='/admin'

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,17 +7,17 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         // Perform login validation and authentication
 
         // Check if the user has previously logged in
-        if (!session()->has('logged_in')) {
-            // Set the success message to be displayed
-            session()->flash('success', 'Welcome back! You have successfully logged in.');
-            // Set the logged_in session variable to true
-            session()->put('logged_in', true);
-        }
+        // if (!session()->has('logged_in')) {
+        //     // Set the success message to be displayed
+        //     session()->flash('success', 'Welcome back! You have successfully logged in.');
+        //     // Set the logged_in session variable to true
+        //     session()->put('logged_in', true);
+        // }
 
         return view('admin.dashboard');
     }
--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,11 @@ Route::get('/', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware(['auth', 'verified'])
-    ->name('admin.')
-    ->prefix('admin')
-    ->group(function () {
-        Route::get('/', [DashboardController::class, 'index'])
-            ->name('dashboard');
-        Route::resource('products', ProductController::class)->parameters(['products'=>'product:slug']);
-    });
+Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])
+        ->name('dashboard');
+    Route::resource('products', ProductController::class)->parameters(['products' => 'product:slug']);
+});
 
 
 // Route::middleware('auth')->group(function () {
@@ -40,4 +37,4 @@ Route::middleware(['auth', 'verified'])
 //     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 // });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
