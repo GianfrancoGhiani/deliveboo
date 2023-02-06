5513b7c1897c71ac2287cb58d81d9d85c3dffe05
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2023-02-06 11:49:33 +0100

    tolto import productController

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\Admin\ProductController;
 use App\Models\Product;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
@@ -15,7 +14,7 @@ class ProductController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+    
      */
     public function index()
     {
@@ -27,7 +26,7 @@ class ProductController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+    
      */
     public function create()
     {
@@ -38,7 +37,7 @@ class ProductController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\StoreProductRequest  $request
-     * @return \Illuminate\Http\Response
+    
      */
     public function store(StoreProductRequest $request)
     {
@@ -60,7 +59,7 @@ class ProductController extends Controller
      * Display the specified resource.
      *
      * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
+    
      */
     public function show(Product $product)
     {
@@ -71,7 +70,7 @@ class ProductController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
+    
      */
     public function edit(Product $product)
     {
@@ -82,7 +81,7 @@ class ProductController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
+     
      */
     public function update(UpdateProductRequest $request, Product $product)
     {
@@ -102,7 +101,7 @@ class ProductController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
+     
      */
     public function destroy(Product $product)
     {
