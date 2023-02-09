117f2bda32f7d826378928938b3be6cba661a759
mattia.rollo <ste.tia@hotmail.it>
2023-02-09 23:48:50 +0100

    modified OrderSeeder

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -18,8 +18,23 @@ class OrderSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-         //DB::statement("SET foreign_key_checks = 0;");
-         //Order::truncate();
+        //DB::statement("SET foreign_key_checks = 0;");
+        //Order::truncate();
+        for ($i = 0; $i < 3; $i++) {
+            $neworder = new Order();
+            $neworder->customer_firstname = $faker->name();
+            $neworder->customer_lastname = $faker->lastName();
+            $neworder->customer_email = $faker->email();
+            $neworder->customer_address = $faker->address();
+            $neworder->customer_tel = $faker->e164PhoneNumber();
+            $neworder->price = $faker->randomFloat(2, 50, 500);
+            $neworder->paid = $faker->numberBetween(0, 1);
+            $neworder->description = $faker->text();
+            $neworder->restaurant_id = 2;
+            $neworder->save();
+            $neworder->products()->attach([1, 4, 7, 10]);
+            // $neworder->products()->attach([30, 31, 32, 33]);
+        }
         for ($i = 0; $i < 3; $i++) {
             $neworder = new Order();
             $neworder->customer_firstname = $faker->name();
@@ -35,6 +50,6 @@ class OrderSeeder extends Seeder
             //$neworder->products()->attach([1, 4, 7, 10]);
             $neworder->products()->attach([30, 31, 32, 33]);
         }
-       // DB::statement("SET foreign_key_checks = 1;");
+        // DB::statement("SET foreign_key_checks = 1;");
     }
 }
