074ebb6caf16b35eb4aa0c6d6b029f100c977c69
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2023-02-15 10:20:56 +0100

    order requste wiht quantity

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -13,7 +13,7 @@ class OrderController extends Controller
 {
     public function generate(Request $request, Gateway $gateway)
     {
-        // dd($gateway->clientToken()->generate());
+        // dd($gateway);
 
         $token = $gateway->clientToken()->generate();
         $data = [
@@ -24,7 +24,8 @@ class OrderController extends Controller
     }
     public function makePayment(OrderRequest $request, Gateway $gateway)
     {
-        $amount = OrderController::totalPrice($request->products);
+        // dd($request);
+        $amount = OrderController::totalPrice($request->products)['amount'];
 
         $result = $gateway->transaction()->sale([
             'amount' =>  intval($amount),
@@ -33,34 +34,73 @@ class OrderController extends Controller
                 'submitForSettlement' => true
             ]
         ]);
-        // dd($result);
+        $restaurantId = OrderController::totalPrice($request->products)['restaurantId'];
+        // dd($request);
         if ($result->success) {
             $data = [
                 'success' => true,
-                'message' => 'Payment done correctly'
+                'message' => 'Payment done correctly!'
             ];
+            // $description =
+            //     " 
+            // {$data['message']} 
+            // Thank you {$request->customerData['customer_firstname']} { $request->customerData['customer_lastname']}.
+            // You have ordered:
+
+
+
+            // -------
+            // Products list
+            // -------
+
+
+            // ";
+
             return response()->json($data);
         } else {
             $data = [
                 'success' => false,
-                'message' => 'Payment refused'
+                'message' => 'Payment refused!'
             ];
+            // $description = $data['message'];
+
             return response()->json($data);
         }
     }
 
-    public static function totalPrice($productsIdsArray)
+    public static function totalPrice($productsIdsQuantityArray)
     {
         $totalPrice = 0;
-        foreach ($productsIdsArray as $product_id) {
-            $product = Product::find($product_id);
+        foreach ($productsIdsQuantityArray as $tempProduct) {
+            $prodQuantity = $tempProduct['q'];
+            $product = Product::find($tempProduct['id']);
+
+            $restaurantId = $product->restaurant_id;
             $discount = $product->discount;
             if ($discount) {
-                $totalPrice += ($product->price - (($product->price / 100) * $discount));
+                $totalPrice += (($product->price - (($product->price / 100) * $discount))) * $prodQuantity;
             } else {
-                $totalPrice += $product->price;
+                $totalPrice += $product->price * $prodQuantity;
             }
         }
-        return $totalPrice;
+        return [
+            'amount' => $totalPrice,
+            'restaurantId' => $restaurantId
+        ];
+    }
+
+    public static function storeOrder($formData, $totalPrice, $paid, $r_id, $descr)
+    {
+        $neworder = new Order();
+        $neworder->customer_firstname = $formData['customer_firstname'];
+        $neworder->customer_lastname = $formData['customer_lastname'];
+        $neworder->customer_email = $formData['customer_email'];
+        $neworder->customer_address = $formData['customer_address'];
+        $neworder->customer_tel = $formData['customer_tel'];
+        $neworder->price = $formData['$totalPrice'];
+        $neworder->paid = $formData['$paid'];
+        $neworder->description = $descr;
+        $neworder->restaurant_id = $r_id;
+        $neworder->save();
     }
 }
